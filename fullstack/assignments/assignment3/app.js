f1c3234e5ba5a1d048069220275164c77a470e14
dazas <doni.koketna@gmail.com>
2017-09-09 22:35:18 +0300

    Edit

--- fullstack/assignments/assignment3/app.js
+++ fullstack/assignments/assignment3/app.js
@@ -8,46 +8,6 @@
 
     NarrowItDownController.$inject = ['MenuSearchService'];
 
-    function NarrowItDownController(MenuSearchService) {
-        var menu = this;
-        menu.shortName = '';
-
-        menu.matchedMenuItems = function(searchTerm) {
-            var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
-
-            promise.then(function(items) {
-                if (items && items.length > 0) {
-                    menu.message = '';
-                    menu.found = items;
-                } else {
-                    menu.message = 'Nothing found!';
-                    menu.found = [];
-                }
-            });
-        };
-
-        menu.removeMenuItem = function(itemIndex) {
-            menu.found.splice(itemIndex, 1);
-        }
-    }
-
-    function FoundItems() {
-            var ddo = {
-                restrict: 'E',
-                templateUrl: 'foundItems.html',
-                scope: {
-                    foundItems: '<',
-                    onEmpty: '<',
-                    onRemove: '&'
-                },
-                controller: NarrowItDownController,
-                controllerAs: 'menu',
-                bindToController: true
-            };
-
-            return ddo;
-        }
-        
     MenuSearchService.$inject = ['$http'];
 
     function MenuSearchService($http) {
@@ -60,11 +20,7 @@
             }).then(function(response) {
                 var foundItems = [];
 
-                for (var i = 0; i < response.data['menu_items'].length; i++) {
-                    if (searchTerm.length > 0 && response.data['menu_items'][i]['description'].toLowerCase().indexOf(searchTerm) !== -1) {
-                        foundItems.push(response.data['menu_items'][i]);
-                    }
-                }
+              
                 return foundItems;
             });
         };
